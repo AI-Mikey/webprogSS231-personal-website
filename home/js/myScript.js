bd3ffaef524c4efe4fed5fd08a70b3cd452b03fc
apcmmorenze <mmorenze@student.apc.edu.ph>
2024-12-23 21:18:09 +0800

    changed br to h1

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -3,6 +3,6 @@ function myFunction() {
   }
 
   function myFunction2() {
-    const element = document.getElementsByTagName("br");
+    const element = document.getElementsByTagName("h1");
     document.getElementById("demo").innerHTML = 'The text in first <br tag is >(index 0) is: ' + element[0].innerHTML;
   }
